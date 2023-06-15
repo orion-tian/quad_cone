323bcffab0234938341c18552572de1ce232807e
orion-tian <orionwtian@gmail.com>
2023-06-15 16:50:28 -0500

    Non-perfect slanted cone funct - can translate point of cone to make oblique cones - adjusting subdivisions afterwards messes things up - using button instead of slider not as nice

--- quadCone.py
+++ quadCone.py
@@ -13,6 +13,36 @@ def adjAttr(slider, attr, dataType, *args):
     
     cmds.setAttr(coneShapeName + '.' + attr, val) 
 
+def movePoint(floatField, axis, dir, *args):
+    val = cmds.floatField(floatField, q=True, value=True)
+    if dir == 'neg':
+        val = val*-1
+    sel_vtx = cmds.ls(coneName + '.vtx[:]', fl=True)
+    
+    vtx_by_h = {}
+
+    for vtx in sel_vtx:
+        y_coord = cmds.xform(vtx, q=True, ws=True, t=True)[1]
+        if y_coord in vtx_by_h.keys():
+            vtx_by_h[y_coord].append(vtx)
+        else:
+            vtx_by_h[y_coord] = [vtx]
+
+
+    num_layers = float(len(vtx_by_h)) - 1.0
+    i = 0
+    for k in vtx_by_h.keys():
+        for v in vtx_by_h[k]:
+            if i!=0:
+                posi = cmds.xform(v, q=True, ws=True, t=True)
+                if axis == 'x': 
+                    cmds.move(posi[0] + val * i/num_layers, posi[1], posi[2], v, ws=True)
+                elif axis == 'z':
+                    cmds.move(posi[0], posi[1], posi[2] + val * i/num_layers, v, ws=True)
+                else:
+                    print("Argument wrong")
+        i += 1
+
 def createUI(pWindowTitle):
     windowID = "myWindowID"
     
@@ -51,7 +81,30 @@ def createUI(pWindowTitle):
 
     cmds.separator(h=10, style='none')
 
-    cmds.button(label='Cancel', command=cancelCallback)
+
+    cmds.text("Adjust position of top point (Do this last)")
+    
+    cmds.separator(h=10, style='none')
+
+    cmds.rowColumnLayout(numberOfColumns=6, 
+                         columnWidth=[(1,145),(2, 75), (3, 10), (4,65),(5,5),(6,65)],
+                         columnOffset=[(1,"right", 3)])
+    
+    cmds.text(label="X-axis: ")
+    xAxisFloat = cmds.floatField(min = 0.1, max = 5, value = 1, step = 0.1)
+    cmds.separator(h=10, style='none')
+    xAxisDecrButt = cmds.button(label='<-', command=partial(movePoint, xAxisFloat, 'x', 'neg'))
+    cmds.separator(h=10, style='none')
+    xAxisIncrButt = cmds.button(label='->', command=partial(movePoint, xAxisFloat, 'x', 'pos'))
+
+    cmds.text(label="Z-axis: ")
+    zAxisFloat = cmds.floatField(min = 0.1, max = 5, value = 1, step = 0.1)
+    cmds.separator(h=10, style='none')
+    zAxisDecrButt = cmds.button(label='<-', command=partial(movePoint, zAxisFloat, 'z', 'neg'))
+    cmds.separator(h=10, style='none')
+    zAxisIncrButt = cmds.button(label='->', command=partial(movePoint, zAxisFloat, 'z', 'pos'))
+    
+    cmds.separator(h=10, style='none')
     
     cmds.showWindow()
 
